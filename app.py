3be617e62481e38a4f3657af2a6eb2b9ee3e5fe8
Sam Kelly <samkelly5941@gmail.com>
2022-03-29 00:04:30 -0400

    windows compat

--- app.py
+++ app.py
@@ -326,10 +326,10 @@ def run():
             
             if opt.key is None:
                 opt.key = input("Path to credential key: ")
-            if platform.system() == 'Linux':
-                query = f'cat {opt.key} | docker login -u _json_key_base64 --password-stdin https://us-west2-docker.pkg.dev'
-            elif platform.system() == 'Windows':
+            if platform.system() == 'Windows':
                 query = f'docker login -u _json_key_base64 --password-stdin https://us-west2-docker.pkg.dev < {opt.key}'
+            else:
+                query = f'cat {opt.key} | docker login -u _json_key_base64 --password-stdin https://us-west2-docker.pkg.dev'
             os.system(query)
             try:
                 print(f'Downloading Image from Google Cloud Platform with {opt.key} credentials')
